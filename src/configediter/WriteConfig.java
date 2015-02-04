06a2d919a735bfdea700a96b823bf806d7ec7300
Ian Van Schaick <Ian Van Schaick@IanVanSchaick>
2015-02-04 17:42:34 +0800

    WriteConfig file does not work. Commented out the write section, now just prints the first 200 lines of the Arraylist without empty lines before titles.

--- src/configediter/WriteConfig.java
+++ src/configediter/WriteConfig.java
@@ -61,59 +61,76 @@ public class WriteConfig {
             if (index < lines.size()) {
                 sizeData2 = lines.get(index).size();
             }
-            while (sizeData2 > 0) {
+            boolean notEmpty = false;
+            while (sizeData2 > 0 && indexI < sizeData2) {
                 if (lines.get(index).get(indexI).contains("\n")) {
                     lines.get(index).remove(indexI);
                     lines.remove(index);
                     spaces++;
+                    sizeData2--;
                 }
+                
+                if (index <= 200) {
+                    int diff = sizeData2 - indexI;
+                    if (diff == 1) {
+                        System.out.print(lines.get(index).get(indexI)); 
+                    }
+                    else {
+                        System.out.print(lines.get(index).get(indexI) + " "); 
+                    }
+                }
+                
                 indexI++;
+                notEmpty = true;
+            }
+            if (notEmpty) {
+                System.out.println("");
             }
             length--;
-            System.out.println("Index: " + index);
+//            System.out.println("Index: " + index);
             index++;
         }
         
-        index = 1;
-        length = lines.size();
-        File temp = new File(openTTDfolder2 + "\\temp.cfg");
-        BufferedWriter bf = new BufferedWriter(new FileWriter(temp, false));;
-
-        bf.write(lines.get(0).get(0));
-        bf.newLine();
-        int sizeData = lines.get(index).size();
-        while (length > 0) {
-            int indexI = 0;
-            if (index < lines.size()) {
-                sizeData = lines.get(index).size();
-            }
-            while (sizeData > 0) {
-                if (sizeData == 1) {
-                    bf.write(lines.get(index).get(indexI) + "");
-                    sizeData--;
-                    indexI++;
-                } else {
-                    bf.write(lines.get(index).get(indexI) + " ");
-                    sizeData--;
-                    indexI++;
-                }
-            }
-            length--;
-            index++;
-            if (lines.get(index).get(0).contains("[")) {
-                bf.newLine();
-            }
-            System.out.println("Index: " + index);
-            if (!(index == initLength)) {
-                bf.newLine();
-            }
-        }
-        bf.close();
-        success = true;
-        File config = new File(openTTDfolder);
-        boolean delete = config.delete();
-        Path source = FileSystems.getDefault().getPath("", openTTDfolder2 + "\\temp.cfg");
-        Path move = Files.move(source, source.resolveSibling(openTTDfolder));
+//        index = 1;
+//        length = lines.size();
+//        File temp = new File(openTTDfolder2 + "\\temp.cfg");
+//        BufferedWriter bf = new BufferedWriter(new FileWriter(temp, false));;
+//
+//        bf.write(lines.get(0).get(0));
+//        bf.newLine();
+//        int sizeData = lines.get(index).size();
+//        while (length > 0) {
+//            int indexI = 0;
+//            if (index < lines.size()) {
+//                sizeData = lines.get(index).size();
+//            }
+//            while (sizeData > 0) {
+//                if (sizeData == 1) {
+//                    bf.write(lines.get(index).get(indexI) + "");
+//                    sizeData--;
+//                    indexI++;
+//                } else {
+//                    bf.write(lines.get(index).get(indexI) + " ");
+//                    sizeData--;
+//                    indexI++;
+//                }
+//            }
+//            length--;
+//            index++;
+//            if (lines.get(index).get(0).contains("[")) {
+//                bf.newLine();
+//            }
+//            System.out.println("Index: " + index);
+//            if (!(index == initLength)) {
+//                bf.newLine();
+//            }
+//        }
+//        bf.close();
+//        success = true;
+//        File config = new File(openTTDfolder);
+//        boolean delete = config.delete();
+//        Path source = FileSystems.getDefault().getPath("", openTTDfolder2 + "\\temp.cfg");
+//        Path move = Files.move(source, source.resolveSibling(openTTDfolder));
         return success;
     }
 }
