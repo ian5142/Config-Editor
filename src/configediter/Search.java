0c3e1a1d3469c55f18bd4e30884ddba900043643
Ian Van Schaick <Ian Van Schaick@IanVanSchaick>
2015-03-08 23:20:04 +0800

    Created 2 arraylists in Search class, changed all fonts to Arial size 18.

--- src/configediter/Search.java
+++ src/configediter/Search.java
@@ -24,14 +24,20 @@ import java.util.ArrayList;
  * @author Ian Van Schaick
  */
 public class Search {
-    private ArrayList<String> list;
+    private final ArrayList<String> list;
+    private final ArrayList<Integer> indexes;
     
-    public Search () {
+    public Search (ArrayList<Integer> indexList) {
+        indexes = indexList;
+        list = new ArrayList<String>(indexes.size());
         fillArray();
     }
-
+    
+    /**
+     * fills the list ArrayList with names of all variables
+     */
     private void fillArray() {
-        list.add("competitor speed");
+        list.add("competitor speed"); //start of difficulty tab
         list.add("construction cost");
         list.add("difficulty level");
         list.add("disasters");
@@ -43,11 +49,33 @@ public class Search {
         list.add("maximum number of competitors");
         list.add("number of industries");
         list.add("number of towns");
+        list.add("custom town number");
         list.add("quantity of seas and lakes - number of seas and lakes");
         list.add("subsidy multiplier");
         list.add("terrain type");
         list.add("town council tolerance");
         list.add("vehicle breakdowns");
-        list.add("vehicle costs");
+        list.add("vehicle costs"); //end of difficulty tab, start of vehicle tab
+        list.add("disable elrails - disable electric rails");
+        list.add("dynamic engines");
+        list.add("extend vehicle life");
+        list.add("freight trains");
+        list.add("maximum aircraft");
+        list.add("maximum road vehicles");
+        list.add("maximum ships");
+        list.add("maximum train length");
+        list.add("maximum trains");
+        list.add("never expire vehicles");
+        list.add("plane crashes");
+        list.add("plane speed");
+        list.add("road side");
+        list.add("road vehicle acceleration model");
+        list.add("road vehicle slope steepness");
+        list.add("service interval aircraft");
+        list.add("service interval road vehicle");
+        list.add("service interval ships");
+        list.add("service interval trains");
+        list.add("service interval is percent");
+        list.add("train acceleration model"); //end of vehicle tab
     }
 }
