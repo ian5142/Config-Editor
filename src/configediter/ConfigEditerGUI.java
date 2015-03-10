5b44740eaf73eec10385bd6a763ea5cdf68c9f55
Ian Van Schaick <Ian Van Schaick@IanVanSchaick>
2015-03-10 22:24:36 +0800

    Renamed package.

--- src/configediter/ConfigEditerGUI.java
+++ src/configediter/ConfigEditerGUI.java
@@ -2920,6 +2920,9 @@ public class ConfigEditerGUI extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(ConfigEditerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
